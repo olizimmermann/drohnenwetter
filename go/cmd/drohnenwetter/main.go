017c2f2b7d982b775b62656b149a74da21f0018b
olizimmermann <github@ozimmermann.com>
2026-04-03 19:52:02 +0200

    fix osm loading issue

--- go/cmd/drohnenwetter/main.go
+++ go/cmd/drohnenwetter/main.go
@@ -88,7 +88,7 @@ func securityHeadersMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
-		w.Header().Set("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' https://unpkg.com; style-src 'self' 'unsafe-inline' https://unpkg.com; img-src 'self' data: https://*.tile.openstreetmap.org https://uas-betrieb.de; connect-src 'self' https://uas-betrieb.de https://unpkg.com")
+		w.Header().Set("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' https://unpkg.com; style-src 'self' 'unsafe-inline' https://unpkg.com; img-src 'self' data: https://*.tile.openstreetmap.org https://uas-betrieb.de; connect-src 'self' https://uas-betrieb.de https://unpkg.com https://*.tile.openstreetmap.org")
 		next.ServeHTTP(w, r)
 	})
 }
